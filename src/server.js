60bc22c19f5fac149d30db34ebc4590bd6424ea3
MumtazAkhtar <mumtazmohammed08@gmail.com>
2017-11-30 11:23:17 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,266 @@
+//<-----------------Requiring Dependencies------------------>
+const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+
+//<-------connecting to postgres database--------->
+const sequelize = new Sequelize('blogapp', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+    storage: './session.postgres'
+})
+
+app.use(express.static('../public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+//<---------Session Store---------->
+app.use(session({
+    store: new SequelizeStore({
+        db: sequelize,
+        checkExpirationInteral: 15 * 60 * 1000,
+        expiration: 24 * 60 * 60 * 1000
+    }),
+    secret: "safe",
+    saveUninitialized: true,
+    resave: false
+}))
+
+//<----------Defining models----------->
+const User = sequelize.define('users', {
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING
+}, { timestamps: false })
+
+const Post = sequelize.define('posts',{
+    posts: Sequelize.STRING
+})
+
+const Comment = sequelize.define('comments',{
+    comments: Sequelize.STRING
+})
+
+//<------RELATION BETWEEN TABLES--------->
+User.hasMany(Post)
+Post.hasMany(Comment)
+Post.belongsTo(User)
+Comment.belongsTo(Post)
+User.hasMany(Comment)
+
+//<<------------------------Routes------------------------->>
+
+// Default index page
+app.get('/', function(req, res) {
+    let user = req.session.user
+    res.render("index.pug", {message: req.query.message })
+})
+
+// <----------Create new user (Signup)----------->
+app.post('/users', function(req, res) {
+
+    let Name = req.body.name
+    let Email = req.body.email
+    let Password = req.body.password
+
+    console.log(`input details--> ${Name}-->${Email}-->${Password}`)
+
+    User.create({
+        name: Name,
+        email: Email,
+        password: Password
+    }).then(function(user) {
+        req.session.user = user;
+        res.redirect('/?message=' + encodeURIComponent("you are succesfully registered"))
+
+    })
+})
+
+//<----------------(GET) sign up page------------->
+app.get('/users/new', function(req, res) {
+    res.render("signup.pug")
+})
+
+//<----------------(POST) login---------------->
+app.post('/login', function(req, res) {
+    let name = req.body.name;
+    let password = req.body.password;
+    console.log(`input details-->${name}-->${password}`)
+
+    User.findOne({
+        where: {
+            name: name
+        }
+
+    }).then(function(details) {
+
+        if (details !== null && password === details.password) {
+            req.session.user = details;
+            
+                res.redirect('/profile')
+            }
+         else {
+            res.redirect('/?message='+ encodeURIComponent("Invalid Email or Password"))
+        } 
+
+    })
+
+})
+
+
+//<----------render profile page---------->
+app.get('/profile', function(req, res) {
+    const user = req.session.user
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("please login to view your profile"))
+
+    } else {
+        User.findAll()
+            .then(allUsers => {
+
+                User.findOne({
+                    where: {
+                        name: user.name
+                    }
+
+                }).then(function(details) {
+                    Post.findAll({
+                        where: {
+                            userId: details.id
+                        },
+                        include: [{
+                            model: Comment
+                        }]
+                    }).then(posts => {
+
+                        console.log(`all posts-------->${JSON.stringify(posts)}`)
+                        posts = posts.map(data => {
+                            var columns = data.dataValues;
+                            return {
+                                id: columns.id,
+                                posts: columns.posts,
+                                comments: columns.comments
+                            }
+                        })
+
+                        res.render('profile', { user: details, posts: posts, allUsers: allUsers })
+                    })
+                })
+
+            })
+    }
+
+})
+
+
+//<-------Posts(post)------------>
+app.post('/posts', function(req, res) {
+    let post = req.body.post;
+    const user = req.session.user
+    User.findOne({
+        where: {
+            name: user.name
+        }
+    }).then(user => {
+        user.createPost({
+            posts: post
+        })
+        res.redirect('/profile')
+    });
+
+});
+
+//<-------To see all posts--------->
+app.get('/allposts', function(req, res) {
+    const user = req.session.user;
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("please login to view all posts"))
+
+    } else {
+
+        User.findAll()
+
+        .then(users=>{
+
+            Post.findAll({
+            include: [
+                { model: Comment },
+                {model: User}
+            ]
+        }).then(posts => {
+            
+                console.log(`working -----------> ${JSON.stringify(posts)}`);
+                console.log(`comments---------->${JSON.stringify(posts[0].comments)}`);
+            posts = posts.map(allData => {
+                var columns = allData.dataValues;
+                return {
+                    id: columns.id,
+                    posts: columns.posts,
+                    userId: columns.userId,
+                    comments: columns.comments
+                }
+            })
+            
+
+              res.render('allposts', {
+                posts: posts,
+                users: users,
+                name: user
+            });
+
+        })
+        
+
+    })
+            
+
+    }
+
+});
+
+
+
+//<----------write comment(POST)-------------->>
+app.post('/comment/:id',function(req,res){
+    let reqparams = req.params;
+    let comment = req.body.comment;
+    const id = req.session.user.id
+    Post.findOne({
+        where:{
+            id: reqparams.id
+        }
+    }).then(post=>{
+        post.createComment({
+            comments: comment,
+            userId: id
+        })
+        res.redirect('/allposts')
+    });
+});
+
+
+
+//<------------logout------------>
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(error) {
+        if (error) {
+            throw error;
+        }
+        res.redirect('/')
+    });
+})
+
+
+
+sequelize.sync()
+
+//<---PORT--->
+app.listen(4000, function() {
+    console.log("app is listening at port 4000")
+})
